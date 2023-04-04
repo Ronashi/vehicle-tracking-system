df7f22d3ee695c5ec0c9c58c1f445996e2b1bfcd
Reagan Ronald <reaganronashi@gmail.com>
2023-04-04 16:50:59 +0300

    models mod

--- vehicle_tracking_system/tracker/models.py
+++ vehicle_tracking_system/tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.gis.db import models as gis_models
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=50)
+    license_plate = models.CharField(max_length=10)
+
+class GPSData(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    location = gis_models.PointField()
