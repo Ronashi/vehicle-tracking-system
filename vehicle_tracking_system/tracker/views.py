a9bbac0aea7fe5821d4c8800880a6099cf511603
Reagan Ronald <reaganronashi@gmail.com>
2023-04-04 16:55:12 +0300

    views mod

--- vehicle_tracking_system/tracker/views.py
+++ vehicle_tracking_system/tracker/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Vehicle, GPSData
+
+def vehicle_map(request, vehicle_id):
+    vehicle = Vehicle.objects.get(id=vehicle_id)
+    latest_gps_data = GPSData.objects.filter(vehicle=vehicle).latest('timestamp')
+    context = {
+        'vehicle': vehicle,
+        'location': latest_gps_data.location,
+    }
+    return render(request, 'vehicle_map.html', context)
