8906825c0a92055905980f43899c1bfe9e3656ae
Reagan Ronald <reaganronashi@gmail.com>
2023-04-04 16:58:45 +0300

    views mod

--- vehicle_tracking_system/vehicle_tracking_system/urls.py
+++ vehicle_tracking_system/vehicle_tracking_system/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vehicle/<int:vehicle_id>/map/', views.vehicle_map, name='vehicle_map'),
 ]
